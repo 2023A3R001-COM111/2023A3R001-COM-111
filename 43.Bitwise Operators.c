c513ac8d391909b20b679ace0be8c21b9a1914c0
Shivam Sharma <146350171+2023A3R001-COM111@users.noreply.github.com>
2023-12-03 17:46:25 +0530

    Update 43.Bitwise Operators.c

--- 43.Bitwise Operators.c
+++ 43.Bitwise Operators.c
@@ -2,15 +2,15 @@
 int main()
 {
 //Demonstrating the working of bitwise operators 
-     int a,b;
+     int x,y;
      printf("Enter two integers : ");
-     scanf("%d %d",&a , &b);
-     printf("The result of bitwise AND operator is a & b = %d\n", a&b);
-     printf("The result of bitwise OR operator is  a | b = %d\n", a|b);
-     printf("The result of bitwise XOR operator is  a ^ b = %d\n", a^b);
-     printf(" ~a = %d\n", ~a);
-     printf("~b = %d\n", ~b);
-     printf("a <<1 = %d\n", a<<1);
-     printf("b >>1 = %d\n", b>>1);
+     scanf("%d %d",&x , &y);
+     printf("The result of bitwise AND operator is x & y = %d\n", a&b);
+     printf("The result of bitwise OR operator is  x | y = %d\n", x|y);
+     printf("The result of bitwise XOR operator is  x ^ y = %d\n", x^y);
+     printf(" ~x = %d\n", ~x);
+     printf("~y = %d\n", ~y);
+     printf("x <<1 = %d\n", x<<1);
+     printf("y >>1 = %d\n", y>>1);
      return 0;
 }
